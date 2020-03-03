f63f27ae2149784ef7e74b6726ebd48391a0d833
songtham <songtham@DEMD534.wagner-group.eu>
2020-03-03 16:58:18 +0100

    Habe es zum laufen gebracht! :-)

--- src/main/java/osgi/gui/ob/tab/MyTab.java
+++ src/main/java/osgi/gui/ob/tab/MyTab.java
@@ -4,19 +4,25 @@ import com.dscsag.plm.spi.interfaces.ECTRService;
 import com.dscsag.plm.spi.interfaces.gui.ob.PluginObjectBrowserTab;
 import com.dscsag.plm.spi.interfaces.objects.ObjectData;
 import com.dscsag.plm.spi.interfaces.objects.PlmObjectKey;
-import com.dscsag.plm.spi.interfaces.objects.doc.DocumentData;
 import com.dscsag.plm.spi.interfaces.progress.ProgressEvent;
 import com.dscsag.plm.spi.interfaces.progress.ProgressTracker;
 import com.dscsag.plm.spi.interfaces.rfc.RfcResult;
 import com.dscsag.plm.spi.rfc.builder.RfcCallBuilder;
 
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class MyTab implements PluginObjectBrowserTab<RfcResult>
 {
-	//private JLabel label;
+  private JLabel label;
   private JPanel panel;
+  private JTextField no1;
+  private String test;
   private ECTRService ectrService;
+  
 
   MyTab(ECTRService ectrService)
   {
@@ -24,17 +30,33 @@ public class MyTab implements PluginObjectBrowserTab<RfcResult>
     
     panel = new JPanel();
     
-    JLable label = new JLabel("test1");
+    label = new JLabel("test1");
     panel.add(label);
     
-    JButton b1 = new Button("Button");
+    Button b1 = new Button("Button");
     panel.add(b1);
    
-    JTextField no1 = new JTextField(30);
+    no1 = new JTextField(30);
     panel.add(no1);
-	panel.setVisible(true);  
+	panel.setVisible(true);
+	
+	b1.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			selectionButtonPressed();
+		}
+	} );
     
     this.ectrService=ectrService;
+  }
+  
+  public void selectionButtonPressed() 
+  {
+	test = no1.getText();
+	RfcCallBuilder callBuilder = new RfcCallBuilder("Z_FIS_FIELD_MAINTANANCE");	
+	callBuilder.setInputParameter("ZZ_FIELD1",test);
+	ectrService.getRfcExecutor().execute(callBuilder.toRfcCall());
+
+	//JOptionPane.showMessageDialog(panel, test, "Test", JOptionPane.INFORMATION_MESSAGE, null);
   }
 
   @Override
@@ -65,21 +87,38 @@ public class MyTab implements PluginObjectBrowserTab<RfcResult>
 	return getType();
   }
 
-  @Override
+@Override
+public void clear() {
+	// TODO Auto-generated method stub
+	
+}
+
+@Override
+public RfcResult fetchData(PlmObjectKey arg0, ProgressTracker arg1) {
+	// TODO Auto-generated method stub
+	return null;
+}
+
+@Override
+public void updateUI(RfcResult arg0) {
+	// TODO Auto-generated method stub
+	
+}
+  
+
+  /*@Override
   public RfcResult fetchData(PlmObjectKey plmObjectKey, ProgressTracker tracker)
   {
+	test = no1.getText();
+	//JOptionPane.showMessageDialog(panel, test, "Test", JOptionPane.INFORMATION_MESSAGE, null);
     ectrService.getPlmLogger().trace("dataFetcher...");
     tracker.updateProgress(new ProgressEvent("calling SAP..."));
-    RfcCallBuilder callBuilder = new RfcCallBuilder("/DSCSAG/DOC_GETDETAIL2");
-    callBuilder.setInputParameter("DOCUMENTTYPE",getType());
-    callBuilder.setInputParameter("DOCUMENTNUMBER",plmObjectKey.getKey());
-    callBuilder.setInputParameter("DOCUMENTPART",plmObjectKey.getKey());
-    callBuilder.setInputParameter("DOCUMENTVERSION",plmObjectKey.getKey());
-    callBuilder.setInputParameter("GETCLASSIFICATION","X");
+    RfcCallBuilder callBuilder = new RfcCallBuilder("Z_FIS_FIELD_MAINTANANCE");	
+    callBuilder.setInputParameter("ZZ_FIELD1",test);
     return ectrService.getRfcExecutor().execute(callBuilder.toRfcCall());
   }
 
-  @Override
+ /* @Override
   public void updateUI(RfcResult executeResult)
   {
     ectrService.getPlmLogger().trace("updateUI...");
@@ -91,6 +130,7 @@ public class MyTab implements PluginObjectBrowserTab<RfcResult>
   public void clear()
   {
     label.setIcon(null);
-    label.setText("");
-  }
+    label.setText("Test");
+    panel.add(label);
+  }*/
 }
